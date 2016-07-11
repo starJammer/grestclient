ff9aa3805a100619eba141c4257daacac5f25a98
starJammer <SaraviaJ@gmail.com>
2016-07-11 01:28:28 -0400

    Add some basic doc.go

--- doc.go
+++ doc.go
@@ -1,1 +1,20 @@
+/*
+Example use:
+
+	import (
+		"net/url"
+	)
+
+	base, err := url.Parse("http://example.com")
+	c, err := New(base)
+
+	c.Get(&Params{
+		Path: "path/to/resource",
+		Headers:
+		Query:
+		Body:
+		unmarshalerMap:
+	})
+
+*/
 package grestclient
