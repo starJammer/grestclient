85ad4d3789778131bff268eadc0287a657ea80dc
starJammer <SaraviaJ@gmail.com>
2016-07-11 01:32:24 -0400

    Alter doc.go

--- doc.go
+++ doc.go
@@ -15,7 +15,20 @@ Example use:
 		Path: "path/to/resource",
 		Headers: http.Header{"One-Off": []string{"one"}},
 		Query: url.Values{"testquery": []string{"test-override"}},
-		Body:         "hello",
+		UnmarshalMap: UnmarshalMap{
+			200: &success,
+			201: &otherSuccess,
+			202: &otherSuccess,
+			400: &fail,
+			404: &fail,
+		},
+	})
+
+	c.Post(&Params{
+		Path: "path/to/resource",
+		Headers: http.Header{"One-Off": []string{"one"}},
+		Query: url.Values{"testquery": []string{"test-override"}},
+		Body: "hello",
 		UnmarshalMap: UnmarshalMap{
 			200: &success,
 			201: &otherSuccess,
