517482c801691c3e8ac6e00d848976d69873b4f3
starJammer <SaraviaJ@gmail.com>
2016-07-11 01:31:13 -0400

    Alter doc.go slightly

--- doc.go
+++ doc.go
@@ -8,12 +8,21 @@ Example use:
 	base, err := url.Parse("http://example.com")
 	c, err := New(base)
 
+	var success string
+	var otherSuccess string
+	var fail string
 	c.Get(&Params{
 		Path: "path/to/resource",
-		Headers:
-		Query:
-		Body:
-		unmarshalerMap:
+		Headers: http.Header{"One-Off": []string{"one"}},
+		Query: url.Values{"testquery": []string{"test-override"}},
+		Body:         "hello",
+		UnmarshalMap: UnmarshalMap{
+			200: &success,
+			201: &otherSuccess,
+			202: &otherSuccess,
+			400: &fail,
+			404: &fail,
+		},
 	})
 
 */
